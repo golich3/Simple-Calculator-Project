cbfa4933c21ebae972ffcc836f4e0c1993b916b8
Fatemeh Kashefi <fatemeh.kashefi@gmail.com>
2015-10-31 08:52:07 +0330

    ActionListener aded for all buttons. String field added.

--- src/BtnPanel.java
+++ src/BtnPanel.java
@@ -1,58 +1,122 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class BtnPanel extends JPanel {
- 
+
+	private String txt;
+
+	public String getTxt() {
+		return txt;
+	}
+
 	public BtnPanel() {
 		setLayout(new GridBagLayout());
 		setBounds(10, 10, 250, 300);
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
-		JButton btn1 = new JButton("1");
+		final JButton btn1 = new JButton("1");
 		c.weightx = 0.1;
 		c.weighty = 0.1;
 		c.gridx = 0;
 		c.gridy = 0;
 		c.anchor = GridBagConstraints.WEST;
 		add(btn1, c);
-		
-		JButton btn2 = new JButton("2");
+		btn1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btn1.getText();
+			}
+		});
+
+		final JButton btn2 = new JButton("2");
 		c.gridx = 1;
 		c.gridy = 0;
 		add(btn2, c);
-		
-		JButton btn3 = new JButton("3");
-		
+		btn2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btn2.getText();
+			}
+		});
+
+		final JButton btn3 = new JButton("3");
 		c.gridx = 0;
 		c.gridy = 1;
 		add(btn3, c);
-		
-		JButton btn4 = new JButton("4");
+		btn3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btn3.getText();
+			}
+		});
+
+		final JButton btn4 = new JButton("4");
 		c.gridx = 1;
 		c.gridy = 1;
 		add(btn4, c);
-		
-		JButton btnAdd = new JButton("+");
+		btn4.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btn4.getText();
+			}
+		});
+
+		final JButton btnAdd = new JButton("+");
 		c.gridx = 0;
 		c.gridy = 2;
 		add(btnAdd, c);
-		
-		JButton btnMul = new JButton("*");
+		btnAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btnAdd.getText();
+			}
+		});
+
+		final JButton btnMul = new JButton("*");
 		c.gridx = 1;
 		c.gridy = 2;
 		add(btnMul, c);
-		
-		JButton btnDiv = new JButton("/");
+		btnMul.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btnMul.getText();
+			}
+		});
+
+		final JButton btnDiv = new JButton("/");
 		c.gridx = 0;
 		c.gridy = 3;
 		add(btnDiv, c);
-		
-		JButton btnEqual = new JButton("=");
+		btnDiv.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btnDiv.getText();
+			}
+		});
+
+		final JButton btnEqual = new JButton("=");
 		c.gridx = 1;
 		c.gridy = 3;
 		add(btnEqual, c);
+		btnEqual.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				txt += btnEqual.getText();
+			}
+		});
 
 	}
 }
